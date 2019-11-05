e67bcd784e92111a05acd4e97f691d38dd41238c
qinWenMeng <qwm668@qq.com>
2019-11-06 00:34:22 +0800

    --update:app.js

---
+++ ts-lesson/app.js
@@ -0,0 +1,13 @@
+"use strict";
+// Basic Types:
+// number
+var num = 123;
+// boolean
+var flag = true;
+// string
+var str = 'Hello, TypeScript!';
+// elemType[], Array<elemType>
+var arr = [1, 2, 3];
+var list = ['one', 'two', 'three'];
+// Tuple
+var tuple = [123, 'Hello, TypeScript!'];
